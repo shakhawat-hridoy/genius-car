871bf9f8793e3c10f1d36538e77622f2d968d816
shakhawat-hridoy <shakhawat.hridoy56@gmail.com>
2022-04-15 16:39:51 +0600

    set environment variable for security

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,29 +1,48 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../Register/Register.css";
 
-const Register = () => {
+const Register = (event) => {
+  const navigate = useNavigate();
+  const navigateLogin = () => {
+    navigate("/login");
+  };
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+  };
   return (
     // Our own created form for learning environment variable
 
     <div className="register-form">
-      <form>
-        <input type="text" name="Your name" placeholder="Your name" required />
+      <h2 style={{ textAlign: "center" }}>Please Register</h2>
+      <form onSubmit={handleRegister}>
+        <input type="text" name="name" placeholder="Your name" required />
 
-        <input
-          type="email"
-          name="Your Email"
-          placeholder="Enter email"
-          required
-        />
+        <input type="email" name="email" placeholder="Enter email" required />
 
         <input
           type="password"
-          name="Enter password"
+          name="password"
           placeholder="Enter password"
           required
         />
         <br />
         <input type="submit" value="Register" />
       </form>
+      <p>
+        Already have an account?{" "}
+        <span
+          style={{ cursor: "pointer" }}
+          className="text-primary"
+          onClick={navigateLogin}
+        >
+          Please Login
+        </span>
+      </p>
     </div>
   );
 };
